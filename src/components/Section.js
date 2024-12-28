13416932aa700b615483a4eab55883e7ec77518b
aryyyn <aryan.211618@ncit.edu.np>
2024-12-28 16:40:45 +0545

    modified section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,8 @@ import Image from "next/image"
 
 const Section = ({title}) => {
     return (
-<div className="w-full border-4 border-nosk-yellow bg-nosk-green p-2 text-center flex justify-between items-center">
+<div className="w-full border-4 border-nosk-yellow bg-nosk-green p-2 text-center flex justify-between items-center max-sm:flex-col">
+<div className="max-sm:hidden">
   <Image
     src="assets/svg/Wire_Long.svg"
     width={300}
@@ -12,9 +13,10 @@ const Section = ({title}) => {
     alt="Left Wire"
     className="transform translate-x-[-30px]"
   />
+  </div>
 
-  <p className="text-nosk-white text-6xl font-bold">{title}</p>
-
+  <span className="text-nosk-white text-3xl sm:text-6xl font-bold max-sm:mt-1 max-sm:mb-1">{title}</span>
+  <div className="max-sm:hidden">
   <Image
     src="assets/svg/Wire_Long.svg"
     width={300}
@@ -23,6 +25,9 @@ const Section = ({title}) => {
     className="transform scale-x-[-1] translate-x-[30px]"
   />
 </div>
+</div>
+
+
 
     )
 }
