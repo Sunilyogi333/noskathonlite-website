f0a65100cf9c5655f610cce6a59cf54cc85fd84c
IntelligentBeaver <aman.211506@ncit.edu.np>
2024-12-28 16:22:01 +0545

    Slight fixes to hero page and button size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,29 +4,26 @@ import { Button } from "./Button.js";
 const HeroSection = () => {
   return (
     <div className="h-screen hero-section text-white w-full flex items-center justify-center min-h-screen text-center">
-      <div className="flex flex-col md:flex-row lg:flex-row md:12 lg:gap-20 items-center justify-center lg:mt-8 w-full">
+      <div className="flex flex-col md:flex-row md:12 max-w-8xl lg:gap-20 items-center justify-between lg:mt-8 w-full">
+        {/* The content section */}
         <div className="text-center lg:text-left flex-col items-center justify-center">
-          <div className="text-4xl font-bold md:text-5xl lg:text-5xl mb-4 gap-2 w-full flex flex-col items-center justify-center lg:flex-row">
-            <p className="text-nosk-black text-center">
-              NOSKATHON
-            </p>
-            <p className="bg-nosk-green inline-block text-nosk-white text-center">
+          <div className="font-bold mb-4 gap-2 w-full flex flex-col items-center justify-center lg:flex-row">
+            <h1 className="text-nosk-black text-center">NOSKATHON</h1>
+            <h1 className="bg-nosk-green inline-block p-2 text-nosk-white text-center">
               LITE
-            </p>
+            </h1>
           </div>
 
-          <p className="text-2xl md:text-4xl lg:text-7xl mb-6 font-normal text-center">
-            2025
-          </p>
-          <p className="text-center text-sm md:text-lg lg:text-xl text-nosk-green font-bold mb-8">
+          <h1 className="mb-6 font-normal text-center">2025</h1>
+          <h5 className="text-center text-nosk-green font-bold mb-8">
             10<sup>th</sup> January, 2025 - 11<sup>th</sup> January, 2025
-          </p>
+          </h5>
 
           <div className="relative flex items-center justify-center">
             <Button
               value="REGISTER NOW"
               link="/register"
-              className="bg-nosk-green text-nosk-white text-2xl lg:text-4xl font-bold py-4 px-6 relative z-10"
+              className="bg-nosk-green text-nosk-white text-xl sm:text-2xl md:text-3xl lg:text-4xl lg:font-bold py-4 px-6 relative z-10"
             />
 
             {/* <div className="absolute right-28 top-0 transform translate-x-1/2 ">
