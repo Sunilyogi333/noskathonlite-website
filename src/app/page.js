c9c10684aa2c797990ea8f0e056425f2651758e5
Sanjib Shah <sanjib.211533@ncit.edu.np>
2024-12-29 11:44:27 +0545

    team_section

--- src/app/page.js
+++ src/app/page.js
@@ -2,43 +2,26 @@
 
 import CountDown from "@/components/CountDown";
 import Header from "../components/Header";
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-import Faq from "@/components/Faq";
-import About from "@/components/About";
-import Section from "@/components/Section";
-import Team from "@/components/Team";
+import HeroSection from "@/components/Hero";
 import Domain from "../components/Domain";
 import TeamSection from "../components/TeamSection";
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
       {/* Fixed Header */}
-      <div className="fixed top-0 left-0 w-full z-50 overflow-hidden">
+      <div className="flex-none">
         <Header />
       </div>
 
       {/* Scrollable Content */}
-      <div className="flex-1 overflow-y-auto mt-38 overflow-hidden">
-        <Hero />
-
-        <Section title="ABOUT" />
-        <About />
-
-        <Section title="FAQs" />
-        <Faq
-          question={"which is the Best team from OC??"}
-          answer={"Tech Team"}
-        />
-
-        <Section title="OUR TEAM" />
-        <Team />
-
-        <Footer />
+      <div className="flex-1 overflow-y-auto px-8 md:px-10 lg:px-16">
+        <HeroSection />
+        <Domain />
+        
       </div>
 
       {/* Fixed Countdown */}
-      <div className="sticky bottom-0 w-full z-50 ">
+      <div className="flex-none">
         <CountDown />
       </div>
     </div>
