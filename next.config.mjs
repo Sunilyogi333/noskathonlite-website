66b511c522d74401266021023c5662d4e3dfe904
IntelligentBeaver <aman.211506@ncit.edu.np>
2024-12-31 20:03:54 +0545

    added deploy actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static export mode
+  basePath: "/noskathonlite-website", // Replace <repository-name> with the GitHub repo name
+  assetPrefix: "/noskathonlite-website", // Prefix for static assets
+};
 
 export default nextConfig;
