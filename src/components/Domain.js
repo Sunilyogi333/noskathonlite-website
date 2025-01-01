3e14034965997a101418efe93ad42a360448b7ce
IntelligentBeaver <aman.211506@ncit.edu.np>
2025-01-02 00:08:13 +0545

    Improved Domain Page.

--- src/components/Domain.js
+++ src/components/Domain.js
@@ -7,19 +7,19 @@ const Domain = () => {
 
   // Sections data
   const sections = [
-    { id: 1, title: "Open Source", image: "/assets/domain/open_souce.jpg" },
-    { id: 2, title: "AI for Good", image: "/assets/domain/AI.jpg" },
+    { id: 1, title: "Open Source", image: "/assets/theme1.jpg" },
+    { id: 2, title: "AI for Good", image: "/assets/theme2.jpg" },
     {
       id: 3,
       title: "Climate Change Through Technology",
-      image: "/assets/domain/climate.jpg",
+      image: "/assets/theme3.jpg",
     },
   ];
 
   return (
-    <div className="flex flex-col items-center space-y-5 my-8">
+    <div className="flex flex-col mx-auto items-center max-w-9xl space-y-5 my-8">
       <div className="w-full md:w-2/3">
-        <div className="flex flex-wrap md:flex-nowrap space-x-2 p-4 h-[500px]">
+        <div className="flex flex-wrap md:flex-nowrap space-x-2 p-4 h-[600px]">
           {sections.map((section) => (
             <div
               key={section.id}
@@ -44,11 +44,11 @@ const Domain = () => {
                     className="flex items-center justify-center font-bold"
                     style={{
                       borderRadius: "50%",
-                      border: "2px solid white",
+                      border: "3px solid white",
                       color: "white",
-                      width: "2rem",
-                      height: "2rem",
-                      lineHeight: "2rem",
+                      width: "3rem",
+                      height: "3rem",
+                      lineHeight: "3rem",
                       textAlign: "center",
                     }}
                   >
@@ -58,7 +58,7 @@ const Domain = () => {
               )}
               {/* Expandable Content */}
               {activeSection === section.id && (
-                <div className="absolute bottom-0 left-0 right-0 p-4 bg-nosk-white bg-opacity-30 backdrop-blur-md text-white text-sm md:text-base flex items-center transition-all duration-1000">
+                <div className="absolute bottom-0 left-0 right-0 p-4 bg-nosk-black bg-opacity-35 backdrop-blur-md text-white text-sm md:text-base flex items-center transition-all duration-1000">
                   {/* Number and Title */}
                   <span
                     className="flex items-center justify-center font-bold"
@@ -75,7 +75,7 @@ const Domain = () => {
                   >
                     {section.id}
                   </span>
-                  <span className="font-medium text-nosk-white text-lg md:text-xl">
+                  <span className="font-bold text-nosk-white text-lg md:text-xl">
                     {section.title}
                   </span>
                 </div>
